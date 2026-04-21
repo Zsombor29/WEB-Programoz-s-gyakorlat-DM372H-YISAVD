7f15f04c61a0454cdb769b4d8c0c9878ba8ff481
Zsombor29 <baranyizsomborl2@gmail.com>
2026-04-21 13:28:39 +0200

    fixing database connection

--- logicals/regisztral.php
+++ logicals/regisztral.php
@@ -2,7 +2,7 @@
 if(isset($_POST['felhasznalo']) && isset($_POST['jelszo']) && isset($_POST['vezeteknev']) && isset($_POST['utonev'])) {
     try {
         // Kapcsolódás
-        $dbh = new PDO('mysql:host=localhost;port=3307;dbname=gyakorlat7', 'root', '',
+        $dbh = new PDO('mysql:host=localhost;port=3306;dbname=gyakorlat7', 'root', '',
                 array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
         
